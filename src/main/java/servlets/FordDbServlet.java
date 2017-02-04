59a848227b170e71bf6b6444df3e9b982b457943
Dmitriy <workday2006@gmail.com>
2017-02-04 14:41:22 +0300

    first try add  jsp to my back and app

---
+++ src/main/java/servlets/FordDbServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by dima on 19.01.17.
+ */
+@WebServlet("/ford.dima")
+public class FordDbServlet  extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        /*String varTextA = "Hello World!";
+        request.setAttribute("textA", varTextA);
+        String varTextB = "It JSP.";
+        request.setAttribute("textB", varTextB);*/
+
+        /*String adress = System.getProperty("user.dir");
+        request.setAttribute("textA",adress);*/
+
+        //System.out.println(System.getProperty("user.home"));
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/registation.jsp
@@ -0,0 +1,38 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dima
+  Date: 21.01.17
+  Time: 12:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Sign Up</title>
+    <link href="./css/registration.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="login">
+    <div class="heading">
+        <h2>Sign in</h2>
+        <form action="#">
+
+            <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control" placeholder="Username or email">
+            </div>
+
+            <div class="input-group input-group-lg">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+
+            <button type="submit" class="float">Login</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
